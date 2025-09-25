5b76e245fa30d8a865273dd7d05b6c943950068b
linxn <957685656@qq.com>
2025-09-25 17:00:11 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,8 +1,5 @@
 const CUSTOMER_SITES = {
-	niuniuziyuan: {
-		api: 'https://api.niuniuzy.me/api.php/provide/vod',
-		name: '牛牛视频',
-	},
+	
 	zuidaziyuan: {
 		api: 'http://zuidazy.me/api.php/provide/vod',
 		name: '最大资源',
@@ -18,10 +15,6 @@ const CUSTOMER_SITES = {
 	jisuziyuan: {
 		api: 'https://jszyapi.com/api.php/provide/vod/at/json',
 		name: '极速资源',
-	},
-	1080zyku: {
-		api: 'http://api.1080zyku.com/inc/api.php/provide/vod',
-		name: '1080资源',
 	},
 	liangziziyuan: {
 		api: 'http://cj.lziapi.com/api.php/provide/vod/from/lzm3u8',
@@ -43,37 +36,9 @@ const CUSTOMER_SITES = {
 		api: 'https://tyyszyapi.com/api.php/provide/vod',
 		name: '天涯影视',
 	},
-	guangsuziyuan: {
-		api: 'https://api.guangsuapi.com/api.php/provide/vod/from/gsm3u8',
-		name: '光速资源',
-	},
-	baiduziyuan: {
-		api: 'http://api.apibdzy.com/api.php/provide/vod',
-		name: '百度资源',
-	},
-	modouziyuan: {
-		api: 'https://www.mdzyapi.com/api.php/provide/vod',
-		name: '魔都资源',
-	},
 	doubanziyuan: {
 		api: 'https://caiji.dbzy5.com/api.php/provide/vod/at/josn',
 		name: '豆瓣资源',
-	},
-	maoyanziyuan: {
-		api: 'https://api.maoyanapi.top/api.php/provide/vod',
-		name: '猫眼资源',
-	},
-	shandianziyuan: {
-		api: 'http://sdzyapi.com/api.php/provide/vod/from/sdm3u8',
-		name: '闪电资源',
-	},
-	hongniuziyuan: {
-		api: 'http://hongniuzy2.com/api.php/provide/vod/from/hnm3u8',
-		name: '红牛资源',
-	},
-	suboziyuan: {
-		api: 'https://subocj.com/api.php/provide/vod/at/json',
-		name: '速播资源',
 	}
 };
 
